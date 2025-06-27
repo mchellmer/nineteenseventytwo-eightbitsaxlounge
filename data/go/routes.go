f8bb601e209d547b4d2e579704f3b263e9c812b3
mchellmer <mchellmer@gmail.com>
2025-06-27 14:53:21 +0100

    datainit: enable routes to all service methods

--- data/go/routes.go
+++ data/go/routes.go
@@ -10,15 +10,15 @@ func SetupRoutes(svc CouchService) http.Handler {
 	r := chi.NewRouter()
 
 	// Database-level CRUD (e.g., create or delete a database)
-	// r.Post("/data/{dbname}", CreateDbHandler)
+	r.Post("/data/{dbname}", CreateDbHandler(svc))
 	// r.Delete("/data/{dbname}", DeleteDbHandler)
 	// r.Get("/data/{dbname}", GetDbInfoHandler)
 
 	// Document-level CRUD (in a specific database)
 	r.Get("/data/{dbname}/{id}", GetDocHandler(svc))
-	// r.Post("/data/{dbname}", CreateDocHandler)
-	// r.Put("/data/{dbname}/{id}", UpdateDocHandler)
-	// r.Delete("/data/{dbname}/{id}", DeleteDocHandler)
+	r.Post("/data/{dbname}", CreateDocHandler(svc))
+	r.Put("/data/{dbname}/{id}", UpdateDocHandler(svc))
+	r.Delete("/data/{dbname}/{id}", DeleteDocHandler(svc))
 
 	return r
 }
